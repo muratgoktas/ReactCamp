d6a37de88b4ae3011e68b57fad192ad0a6531d8b
Murat GÖKTAŞ <77151500+muratgoktas@users.noreply.github.com>
2021-06-06 01:52:35 +0300

    React 06.06.2021 saat:01:52
    React 06.06.2021 saat:01:52

--- camp-project/src/layouts/Dashboard.jsx
+++ camp-project/src/layouts/Dashboard.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ProductList from '../pages/ProductList'
 import Categories from './Categories'
+import { Grid } from 'semantic-ui-react'
 
 
 export default function Dashboard() {
     return (
-        <div>     
-            <Categories></Categories>
-            <ProductList></ProductList>
+        <div>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Categories />
+                    </Grid.Column>
+                    <Grid.Column width={1}>
+
+                    </Grid.Column>
+
+                    <Grid.Column width={11}>
+                        <ProductList />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
     )
 }
--- camp-project/src/pages/ProductList.jsx
+++ camp-project/src/pages/ProductList.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import ProductService from '../services/productService'
 
 export default function ProductList() {
+
+    //useState([]) Normal parantez () bunu bir fonksiyon olduğunu gösterir.
+    // useState([]) Köşeli parantez [] ise Array içinde array olduğunu gösterir.
+    // setProducts foncsiyondur. useState() için
+    //products datasıda arraydır useState([]) için
+    // elimizdeki products datanın başlangıcı boş array yani bu []
+    const [products, setProducts] = useState([])
+    //lifecycle hook react yaşam döngüsüne müdehale yapmamızı sağlıyor.
+    useEffect(() =>{
+        let productService =new ProductService()
+        productService.getProduct().then(result=>setProducts(result.data.data))
+    })
+    
     return (
         <div>
             <Table celled>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Header</Table.HeaderCell>
-                        <Table.HeaderCell>Header</Table.HeaderCell>
-                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Product Name</Table.HeaderCell>
+                        <Table.HeaderCell>Unit Price</Table.HeaderCell>
+                        <Table.HeaderCell>Unit in Stock</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                        <Table.HeaderCell>Category Number</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    <Table.Row>
-                        <Table.Cell>
-                            <Label ribbon>First</Label>
-                        </Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                        <Table.Cell>Cell</Table.Cell>
-                    </Table.Row>
+                    {
+                        products.map(product => (
+                            <Table.Row key={product.id}>
+                                <Table.Cell>{product.productName}</Table.Cell>
+                                <Table.Cell>{product.unitPrice}</Table.Cell>
+                                <Table.Cell>{product.unitsInStock}</Table.Cell>
+                                <Table.Cell>{product.quantityPerUnit}</Table.Cell>
+                                <Table.Cell>{product.categoryId}</Table.Cell>
+                            </Table.Row>
+                        ))
+                    }
+
+
                 </Table.Body>
 
                 <Table.Footer>
